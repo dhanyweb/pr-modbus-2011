85a26d097420435d4e793c9089719345be2e3824
pintgyu <pintgyu@gmail.com>
2011-09-20 16:09:05 +0000

    x86 release 1? WTF

--- release/config.h
+++ release/config.h
@@ -11,6 +11,9 @@
 #include <stdint.h>
 #include "parser.h"
 
+#define MODE_TEST
+//#define MODE_RELEASE
+
 #define MODBUS_DEV0_ADDR     0x01
 
 /* ADDRESS MAP */
@@ -26,6 +29,8 @@
 #define ADDR_COSPHISUM	276
 #define ADDR_REALENERGY	416
 /* */
+
+#define MAX_METER_NUMBER 127
 
 typedef struct measured_values {
 	uint16_t UL1;
@@ -45,6 +50,50 @@ typedef struct measured_values {
 	uint16_t REALENERGY;
 } measured_values_t;
 
+typedef struct average_values {
+	double UL1[MAX_METER_NUMBER];
+	double UL2[MAX_METER_NUMBER];
+	double UL3[MAX_METER_NUMBER];
+
+	double IL1[MAX_METER_NUMBER];
+	double IL2[MAX_METER_NUMBER];
+	double IL3[MAX_METER_NUMBER];
+
+	double PSUM[MAX_METER_NUMBER];
+	double QSUM[MAX_METER_NUMBER];
+	double SSUM[MAX_METER_NUMBER];
+
+	double COSPHISUM[MAX_METER_NUMBER];
+
+	void(*null)(int meters);
+	void(*calculate)(int divi);
+
+//	uint16_t REALENERGY;
+} average_values_t;
+
 #define CONFIGFILE "main.cfg"
 
 #endif /* CONFIG_H_ */
+
+/*
+ mysql> DESCRIBE measure_new;
+ +------------+-------------+------+-----+---------+-------+
+ | Field      | Type        | Null | Key | Default | Extra |
+ +------------+-------------+------+-----+---------+-------+
+ | meter_id   | smallint(6) | YES  |     | NULL    |       |
+ | time       | datetime    | YES  |     | NULL    |       |
+ | ul1        | float       | YES  |     | NULL    |       |
+ | ul2        | float       | YES  |     | NULL    |       |
+ | ul3        | float       | YES  |     | NULL    |       |
+ | il1        | smallint(6) | YES  |     | NULL    |       |
+ | il2        | smallint(6) | YES  |     | NULL    |       |
+ | il3        | smallint(6) | YES  |     | NULL    |       |
+ | psum       | smallint(6) | YES  |     | NULL    |       |
+ | qsum       | smallint(6) | YES  |     | NULL    |       |
+ | ssum       | smallint(6) | YES  |     | NULL    |       |
+ | cosphisum  | smallint(6) | YES  |     | NULL    |       |
+ | realenergy | int(11)     | YES  |     | NULL    |       |
+ +------------+-------------+------+-----+---------+-------+
+ 13 rows in set (0.00 sec)
+ */
+
